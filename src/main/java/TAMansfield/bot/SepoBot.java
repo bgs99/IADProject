af74061d484517d8335a87d7d7b120bcd34caae6
Theodore Mansfield <the.black.knight.of.oriflamme@gmail.com>
2018-12-02 22:19:41 +0300

    Minor bot update
    Minor bot upd

--- src/main/java/TAMansfield/bot/SepoBot.java
+++ src/main/java/TAMansfield/bot/SepoBot.java
@@ -13,20 +13,23 @@ public class SepoBot extends AbilityBot {
     private final static String BOT_USER_NAME = "SEPO_BOT";
     private final static String BOT_TOKEN = "673753505:AAEdbx1W5Hzz1a-g3q54po2m7Fx-BMDth3g";;
 	private AgentRepository agentRepository;
+	private MissionRepository missionRepository;
+	private TeamRepository teamRepository;
 
-    public static SepoBot proxyBot(AgentRepository agentRepository){
+    public static SepoBot proxyBot(AgentRepository agentRepository, MissionRepository missionRepository, TeamRepository teamRepository){
 
         DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
 
         botOptions.setProxyHost("54.38.195.161");
         botOptions.setProxyPort(58770);
         botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
-        return new SepoBot(botOptions, agentRepository);
+        return new SepoBot(botOptions, agentRepository, missionRepository, teamRepository);
     }
 
-	private SepoBot(DefaultBotOptions options, AgentRepository agentRepository) {
+	private SepoBot(DefaultBotOptions options, AgentRepository agentRepository, MissionRepository missionRepository, TeamRepository teamRepository) {
 		super(BOT_TOKEN, BOT_USER_NAME, options);
 		this.agentRepository = agentRepository;
+		this.missionRepository = missionRepository;
 	}
 
 	@Override
@@ -81,51 +84,102 @@ public class SepoBot extends AbilityBot {
 	
 	public Ability showMissionList() {
 		return Ability.builder().name("showmissionlist").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+				}).build();
 	}
 
 	public Ability showMisssions() {
-		return Ability.builder().name("showmisssions").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+		return Ability.builder()
+				.name("showmisssions")
+				.info("Show your missions")
+				.locality(Locality.USER)
+				.privacy(Privacy.PUBLIC)
+				.action(ctx -> {
+					int id = agentRepository.findByTelegram(ctx.user().getId()).getId();
+					List<Team> teams = teamRepository.findAllByAgent(id);
+                    List<Integer> ids = new List<Integer>();
+                    for (Team t : teams) {
+                    	if(t.getMission().getStatus() != "Выполнена") ids.add(t.getMission().getId());
+                    }
+
+                    /* Making output and sending it */
+
+                    String msg = new String();
+                    for (Integer i : ids) {
+                    	msg += i;
+                    	msg += "\n";
+                    }
+
+                    silent.send(msg, ctx.chatId());
+				}).build();
 	}
 	
 	public Ability showMission() {
-		return Ability.builder().name("showmission").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+		return Ability.builder()
+			.name("showmission")
+			.locality(Locality.USER)
+			.input(1)
+			.privacy(Privacy.PUBLIC).action(ctx -> {
+				int id = agentRepository.findByTelegram(ctx.user().getId()).getId();
+				int mid = missionRepository.findById()
+
+			}).build();
 	}
 	
 	public Ability setStatus() {
 		return Ability.builder().name("setstatus").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 	
 	public Ability getMission() {
 		return Ability.builder().name("getmission").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 	
 	public Ability requestSupport() {
 		return Ability.builder().name("requestsupport").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 	
 	public Ability requestEquipment() {
 		return Ability.builder().name("requestequipment").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 	
 	public Ability assign() {
 		return Ability.builder().name("assign").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 
 	public Ability approveRequest() {
 		return Ability.builder().name("approverequest").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 
 	public Ability denyRequest() {
 		return Ability.builder().name("denyrequest").locality(Locality.USER)
-				.privacy(Privacy.PUBLIC).action(ctx -> {}).build();
+				.privacy(Privacy.PUBLIC).action(ctx -> {
+
+
+				}).build();
 	}
 }
