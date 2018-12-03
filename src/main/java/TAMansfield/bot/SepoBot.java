c017f94b8936b9187f6c7fc920114184c3f32a7e
Theodore Mansfield <the.black.knight.of.oriflamme@gmail.com>
2018-12-03 05:53:45 +0300

    A lot of stuff ready for testing
    Bot is almost complete and ready for testing.
    TODO: handling of requests (send, approve, deny)

--- src/main/java/TAMansfield/bot/SepoBot.java
+++ src/main/java/TAMansfield/bot/SepoBot.java
@@ -1,6 +1,7 @@
 package TAMansfield.bot;
 
 import bgs.repo.AgentRepository;
+import bgs.model.*;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 import org.telegram.abilitybots.api.objects.Locality;
@@ -15,23 +16,40 @@ public class SepoBot extends AbilityBot {
 	private AgentRepository agentRepository;
 	private MissionRepository missionRepository;
 	private TeamRepository teamRepository;
+	private InfoRequestRepository infoRequestRepository;
+	private SupportRequestRepository supportRequestRepository;
+	private PortraitRepository portraitRepository;
 
-    public static SepoBot proxyBot(AgentRepository agentRepository, MissionRepository missionRepository, TeamRepository teamRepository){
+    public static SepoBot proxyBot(AgentRepository agentRepository, 
+    		MissionRepository missionRepository, 
+    		TeamRepository teamRepository,
+    		PortraitRepository portraitRepository,
+   			InfoRequestRepository infoRequestRepository,
+			SupportRequestRepository supportRequestRepository){
 
         DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
 
         botOptions.setProxyHost("54.38.195.161");
         botOptions.setProxyPort(58770);
         botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
-        return new SepoBot(botOptions, agentRepository, missionRepository, teamRepository);
+        return new SepoBot(botOptions, agentRepository, missionRepository, teamRepository, portraitRepository, infoRequestRepository, supportRequestRepository);
     }
 
-	private SepoBot(DefaultBotOptions options, AgentRepository agentRepository, MissionRepository missionRepository, TeamRepository teamRepository) {
+	private SepoBot(DefaultBotOptions options, 
+			AgentRepository agentRepository, 
+    		MissionRepository missionRepository, 
+    		TeamRepository teamRepository,
+    		PortraitRepository portraitRepository,
+   			InfoRequestRepository infoRequestRepository,
+			SupportRequestRepository supportRequestRepository) {
 		super(BOT_TOKEN, BOT_USER_NAME, options);
 		this.agentRepository = agentRepository;
 		this.missionRepository = missionRepository;
+		this.teamRepository = teamRepository;
+		this.portraitRepository = portraitRepository;
+		this.infoRequestRepository = infoRequestRepository;
+		this.supportRequestRepository = supportRequestRepository;
 	}
-
 	@Override
 	public String getBotUsername() {
 		return BOT_USER_NAME;
@@ -82,10 +100,30 @@ public class SepoBot extends AbilityBot {
                 .build();
 	}
 	
+	
 	public Ability showMissionList() {
 		return Ability.builder().name("showmissionlist").locality(Locality.USER)
 				.privacy(Privacy.PUBLIC).action(ctx -> {
+					Agent agent = agentRepository.findByTelegram(ctx.user().getId());
+					LinkedList<Mission> mission_list = missionRepository.findUnfinished();
+					LinkedList<Integer> ids = new LinkedList<Integer>();
+					
+					for(Mission mission : mission_list) {
+						if(mission.getType().getCharsima() >= portraitRepository.findByAgent(agent).getCharisma()
+								&& mission.getType().getLoyalty() >= portraitRepository.findByAgent(agent).getLoyalty()
+								&& mission.getType().getAgression() >= portraitRepository.findByAgent(agent).getAgression()	
+								) {
+							ids.add(mission.getId());
+						}
+					}
+					
+                    String msg = new String();
+                    for (Integer i : ids) {
+                    	msg += i;
+                    	msg += "\n";
+                    }
 
+                    silent.send(msg, ctx.chatId());
 				}).build();
 	}
 
@@ -97,13 +135,13 @@ public class SepoBot extends AbilityBot {
 				.privacy(Privacy.PUBLIC)
 				.action(ctx -> {
 					int id = agentRepository.findByTelegram(ctx.user().getId()).getId();
-					List<Team> teams = teamRepository.findAllByAgent(id);
-                    List<Integer> ids = new List<Integer>();
+					LinkedList<Team> teams = teamRepository.findAllByAgent(id);
+					LinkedList<Integer> ids = new LinkedList<Integer>();
                     for (Team t : teams) {
                     	if(t.getMission().getStatus() != "Выполнена") ids.add(t.getMission().getId());
                     }
 
-                    /* Making output and sending it */
+                    /* Forming output and sending it */
 
                     String msg = new String();
                     for (Integer i : ids) {
@@ -121,25 +159,61 @@ public class SepoBot extends AbilityBot {
 			.locality(Locality.USER)
 			.input(1)
 			.privacy(Privacy.PUBLIC).action(ctx -> {
-				int id = agentRepository.findByTelegram(ctx.user().getId()).getId();
-				int mid = missionRepository.findById()
-
+				try {
+				Mission mission = missionRepository.findById(Integer.parseInt(ctx.firstArg()));
+				}
+				catch(Exception e) {
+					silent.send("Error while getting mission from DB", ctx.chatId());
+				}
+				String msg = String.format("ID: %s\nTYPE: %s\nDESCRIPTION: %s\nSTATUS: %s\nRESPONSIBLE: %s\n", 
+						mission.getId(), 
+						mission.getType(), 
+						mission.getDescription(), 
+						mission.getStatus(), 
+						mission.getResponsible());
+				silent.send(msg, ctx.chatId());
 			}).build();
 	}
 	
 	public Ability setStatus() {
 		return Ability.builder().name("setstatus").locality(Locality.USER)
 				.privacy(Privacy.PUBLIC).action(ctx -> {
-
-
+					try {
+						Mission mission = missionRepository.findById(Integer.parseInt(ctx.firstArg()));
+						}
+						catch(Exception e) {
+							silent.send("Error while getting mission from DB", ctx.chatId());
+						}
+					mission.setStatus(ctx.secondArg());
 				}).build();
 	}
 	
 	public Ability getMission() {
 		return Ability.builder().name("getmission").locality(Locality.USER)
 				.privacy(Privacy.PUBLIC).action(ctx -> {
-
-
+					try {
+						Agent agent = agentRepository.findByTelegram(ctx.user().getId());
+						Mission mission = missionRepository.findById(Integer.parseInt(ctx.firstArg()));
+						}
+						catch(Exception e) {
+							silent.send("Error while getting mission from DB", ctx.chatId());
+						}
+					try {
+							if(mission.getType().getCharsima() >= portraitRepository.findByAgent(agent).getCharisma()
+								&& mission.getType().getLoyalty() >= portraitRepository.findByAgent(agent).getLoyalty()
+								&& mission.getType().getAgression() >= portraitRepository.findByAgent(agent).getAgression()	
+								) {
+							mission.getTeam.add(agent.getId());
+							silent.send("Successfully added", ctx.chatId());
+							}
+							else {
+								silent.send("You're unsuitable for this mission", ctx.chatId());
+							}
+						}
+					catch(Exception e)
+					{
+						silent.send("Internal error", ctx.chatId());
+					}
 				}).build();
 	}
 	
@@ -163,7 +237,29 @@ public class SepoBot extends AbilityBot {
 		return Ability.builder().name("assign").locality(Locality.USER)
 				.privacy(Privacy.PUBLIC).action(ctx -> {
 
-
+					try {
+							Agent agent = agentRepository.findById(Integer.parseInt(ctx.firstArg()));
+							Mission mission = missionRepository.findById(Integer.parseInt(ctx.secondArg()));
+						}
+					catch(Exception e) {
+							silent.send("Error while accessing DB", ctx.chatId());
+						}
+					try {
+							if(mission.getType().getCharsima() >= portraitRepository.findByAgent(agent).getCharisma()
+								&& mission.getType().getLoyalty() >= portraitRepository.findByAgent(agent).getLoyalty()
+								&& mission.getType().getAgression() >= portraitRepository.findByAgent(agent).getAgression()	
+								) {
+							mission.getTeam.add(agent.getId());
+							silent.send("Successfully assigned", ctx.chatId());
+							}
+							else {
+								silent.send("He's unsuitable for this mission", ctx.chatId());
+							}
+						}
+					catch(Exception e)
+					{
+						silent.send("Internal error", ctx.chatId());
+					}
 				}).build();
 	}
 
